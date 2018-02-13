9f921b2e1ab6dce24e97bb31315154f3086ccbb0
fangle <fangle@fangledeMacBook-Pro.local>
2018-02-13 11:21:24 +0800

    添加jhipster生成项目

---
+++ jhipster-tasks/docs/README.md
@@ -0,0 +1,7 @@
+
+# 第一个jhipster 项目
+
+* yo jhipster
+* mvnw启动项目
+
+
---
+++ jhipster-tasks/src/main/java/com/nick/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.nick.repository;
+
+import com.nick.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
